68e49711b136bcf6e8435b6362ab2967e3f1863f
Manik <manikdevbhagat@gmail.com>
2023-03-09 19:10:16 +0000

    Imported profile model

--- controllers/subscriberController.js
+++ controllers/subscriberController.js
@@ -1,6 +1,5 @@
-// import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
-// import generateToken from '../utils/generateToken.js';
+import Profile from '../models/profileModel';
 
 
 const getSubscriberBoard = asyncHandler(async (req,res)=>{
@@ -11,5 +10,6 @@ const getSubscriberBoard = asyncHandler(async (req,res)=>{
 const getGyms = asyncHandler(async (req,res)=>{
   res.status(200).send("List of gyms");
 });
+
 
 export { getSubscriberBoard, getGyms};
