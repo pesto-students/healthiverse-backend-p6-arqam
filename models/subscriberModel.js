37e5be25209aeb7140f8a86f0cda8e29c9f28e0d
Manik <manikdevbhagat@gmail.com>
2023-03-11 18:49:26 +0000

    Updated subscriber schema

--- models/subscriberModel.js
+++ models/subscriberModel.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
+const { Schema } = mongoose;
 
 const subscriberSchema = new mongoose.Schema(
   {
-    _id: {
-      type: String,
-      required: true,
-    },
+    __id: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
     about: {
       type: String,
     },
